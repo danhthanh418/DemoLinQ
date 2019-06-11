3b1a8606df0789e42effa2390ceb99521586a2e6
thanhd19 <15520798@gm.uit.edu.vn>
2019-06-11 13:12:30 +0700

    done demo linq to dataset and linq to entity

--- LinQToDataSet/Form1.Designer.cs
+++ LinQToDataSet/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace LinQToDataSet
 {
-    partial class Form1
+    partial class frmNhanVien
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,204 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.dGVNhanVien = new System.Windows.Forms.DataGridView();
+            this.txtTuKhoa = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTenNhanVien = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSDT = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.cboChucVu = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dGVNhanVien)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(50, 241);
+            this.btnThem.Margin = new System.Windows.Forms.Padding(2);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(56, 19);
+            this.btnThem.TabIndex = 0;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(188, 241);
+            this.btnXoa.Margin = new System.Windows.Forms.Padding(2);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(56, 19);
+            this.btnXoa.TabIndex = 1;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(117, 241);
+            this.btnSua.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(56, 19);
+            this.btnSua.TabIndex = 2;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Location = new System.Drawing.Point(543, 25);
+            this.btnTimKiem.Margin = new System.Windows.Forms.Padding(2);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(56, 19);
+            this.btnTimKiem.TabIndex = 3;
+            this.btnTimKiem.Text = "Tìm";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            // 
+            // dGVNhanVien
+            // 
+            this.dGVNhanVien.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGVNhanVien.Location = new System.Drawing.Point(286, 67);
+            this.dGVNhanVien.Margin = new System.Windows.Forms.Padding(2);
+            this.dGVNhanVien.Name = "dGVNhanVien";
+            this.dGVNhanVien.RowTemplate.Height = 24;
+            this.dGVNhanVien.Size = new System.Drawing.Size(679, 426);
+            this.dGVNhanVien.TabIndex = 4;
+            this.dGVNhanVien.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dGVNhanVien_CellClick);
+            // 
+            // txtTuKhoa
+            // 
+            this.txtTuKhoa.Location = new System.Drawing.Point(286, 26);
+            this.txtTuKhoa.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTuKhoa.Name = "txtTuKhoa";
+            this.txtTuKhoa.Size = new System.Drawing.Size(253, 20);
+            this.txtTuKhoa.TabIndex = 5;
+            this.txtTuKhoa.TextChanged += new System.EventHandler(this.txtTuKhoa_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 113);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Chức vụ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 69);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Tên nhân viên";
+            // 
+            // txtTenNhanVien
+            // 
+            this.txtTenNhanVien.Location = new System.Drawing.Point(92, 67);
+            this.txtTenNhanVien.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTenNhanVien.Name = "txtTenNhanVien";
+            this.txtTenNhanVien.Size = new System.Drawing.Size(153, 20);
+            this.txtTenNhanVien.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 158);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Số điện thoại";
+            // 
+            // txtSDT
+            // 
+            this.txtSDT.Location = new System.Drawing.Point(92, 155);
+            this.txtSDT.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSDT.Name = "txtSDT";
+            this.txtSDT.Size = new System.Drawing.Size(153, 20);
+            this.txtSDT.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 198);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Địa chỉ";
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.Location = new System.Drawing.Point(92, 196);
+            this.txtDiaChi.Margin = new System.Windows.Forms.Padding(2);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(153, 20);
+            this.txtDiaChi.TabIndex = 12;
+            // 
+            // cboChucVu
+            // 
+            this.cboChucVu.FormattingEnabled = true;
+            this.cboChucVu.Location = new System.Drawing.Point(92, 110);
+            this.cboChucVu.Margin = new System.Windows.Forms.Padding(2);
+            this.cboChucVu.Name = "cboChucVu";
+            this.cboChucVu.Size = new System.Drawing.Size(153, 21);
+            this.cboChucVu.TabIndex = 14;
+            // 
+            // frmNhanVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(976, 504);
+            this.Controls.Add(this.cboChucVu);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDiaChi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSDT);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTenNhanVien);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTuKhoa);
+            this.Controls.Add(this.dGVNhanVien);
+            this.Controls.Add(this.btnTimKiem);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "frmNhanVien";
+            this.Text = "Form Nhân Viên";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dGVNhanVien)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.DataGridView dGVNhanVien;
+        private System.Windows.Forms.TextBox txtTuKhoa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTenNhanVien;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSDT;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.ComboBox cboChucVu;
     }
 }
 
--- LinQToDataSet/Form1.cs
+++ LinQToDataSet/Form1.cs
@@ -2,19 +2,322 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace LinQToDataSet
 {
-    public partial class Form1 : Form
+    public partial class frmNhanVien : Form
     {
-        public Form1()
+        string connectString = Properties.Settings.Default.dbDemoConnectionString;
+        private int manv;
+        public frmNhanVien()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.LoadData();
+            this.LoadComboBoxChucVu();
+        }
+        /// <summary>
+        /// Gán dữ liệu cho combo box Chức vụ
+        /// </summary>
+        private void LoadComboBoxChucVu()
+        {
+            try
+            {
+                DataTable dtChucVu = null;
+                var selectChucVu = "SELECT MACV,TENCV FROM CHUCVU";
+                SqlDataAdapter adapter = new SqlDataAdapter(selectChucVu, connectString);
+                adapter.TableMappings.Add("Table", "ChucVu");
+                DataSet dsChucVu = new DataSet();
+                adapter.Fill(dsChucVu);
+                dtChucVu = dsChucVu.Tables[0];
+                cboChucVu.DataSource = dtChucVu;
+                cboChucVu.DisplayMember = "TENCV";
+                cboChucVu.ValueMember = "MACV";
+                cboChucVu.DropDownStyle = ComboBoxStyle.DropDownList;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Có lỗi xảy ra", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Load dữ liệu mặc định
+        /// </summary>
+        private void LoadData()
+        {
+            
+            var selectNhanVienChucVu = "SELECT * FROM NHANVIEN; SELECT * FROM CHUCVU";
+            SqlDataAdapter adapter = new SqlDataAdapter(selectNhanVienChucVu, connectString);
+            adapter.TableMappings.Add("Table", "NhanVien");
+            adapter.TableMappings.Add("Table1", "ChucVu");
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            //Tạo quan hệ khóa ngoại cho 2 datatable
+            DataRelation dataRelation = ds.Relations.Add("FK_NhanVien_ChucVu",
+                                        ds.Tables["ChucVu"].Columns["MACV"],
+                                        ds.Tables["NhanVien"].Columns["MACV"]);
+            DataTable dtNhanVien = ds.Tables["NhanVien"];
+            DataTable dtChucVu = ds.Tables["ChucVu"];
+            DataTable dtNhanVienChucVu = null;
+            try
+            {
+                //câu truy vấn
+                var nhaVienChucVuQuery = from nv in dtNhanVien.AsEnumerable()
+                                         join cv in dtChucVu.AsEnumerable()
+                                         on nv.Field<int>("MACV") equals cv.Field<int>("MACV")
+                                         orderby cv.Field<double>("HESOLUONG") descending //sắp xếp
+                                         select new
+                                         {
+                                             maNV = nv.Field<int>("MANV"),
+                                             tenNV = nv.Field<string>("TENNV"),
+                                             sdt = nv.Field<string>("SDT"),
+                                             diaChi = nv.Field<string>("DIACHI"),
+                                             maCV = cv.Field<int>("MACV"),
+                                             tenVC = cv.Field<string>("TENCV"),
+                                             heSoLuong = cv.Field<double>("HESOLUONG")
+                                         };
+                dtNhanVienChucVu = ToDataTable(nhaVienChucVuQuery);
+                dGVNhanVien.DataSource = dtNhanVienChucVu;
+                this.SetFormatDataGirdView();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Có lỗi xảy ra", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        /// <summary>
+        /// Định dạng kiểu cho dGVNhanVien
+        /// </summary>
+        private void SetFormatDataGirdView()
+        {
+            dGVNhanVien.Columns[0].Visible = false;
+            dGVNhanVien.Columns[0].HeaderText = "MÃ NV";
+            dGVNhanVien.Columns[1].HeaderText = "TÊN NV";
+            dGVNhanVien.Columns[2].HeaderText = "SĐT";
+            dGVNhanVien.Columns[3].HeaderText = "ĐỊA CHỈ";
+            dGVNhanVien.Columns[4].Visible = false;
+            dGVNhanVien.Columns[4].HeaderText = "MÃ CV";
+            dGVNhanVien.Columns[5].HeaderText = "TÊN CV";
+            dGVNhanVien.Columns[6].HeaderText = "HSL";
+            dGVNhanVien.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+        }
+
+        /// <summary>
+        /// Xử lý sự kiện thay đổi giá trị ô tìm kiếm để truy vấn tìm kiếm
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtTuKhoa_TextChanged(object sender, EventArgs e)
+        {
+            this.HandleSearch(txtTuKhoa.Text);
+        }
+        /// <summary>
+        /// Hàm tìm kiếm
+        /// </summary>
+        /// <param name="text"></param>
+        private void HandleSearch(string tuKhoa)
+        {
+            tuKhoa = tuKhoa.ToLower();
+            var selectNhanVienChucVu = "SELECT * FROM NHANVIEN; SELECT * FROM CHUCVU";
+            SqlDataAdapter adapter = new SqlDataAdapter(selectNhanVienChucVu, connectString);
+            adapter.TableMappings.Add("Table", "NhanVien");
+            adapter.TableMappings.Add("Table1", "ChucVu");
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            //Tạo quan hệ khóa ngoại cho 2 datatable
+            DataRelation dataRelation = ds.Relations.Add("FK_NhanVien_ChucVu",
+                                        ds.Tables["ChucVu"].Columns["MACV"],
+                                        ds.Tables["NhanVien"].Columns["MACV"]);
+            DataTable dtNhanVien = ds.Tables["NhanVien"];
+            DataTable dtChucVu = ds.Tables["ChucVu"];
+            DataTable dtNhanVienChucVu = null;
+            try
+            {
+                //câu truy vấn tìm kiếm
+                var nhaVienChucVuQuery = from nv in dtNhanVien.AsEnumerable()
+                                         join cv in dtChucVu.AsEnumerable()
+                                         on nv.Field<int>("MACV") equals cv.Field<int>("MACV")
+                                         where (
+                                            nv.Field<int>("MANV").ToString().ToLower().Contains(tuKhoa) ||
+                                            RemoveSign4VietnameseString(nv.Field<string>("TENNV").ToString().ToLower()).Contains(RemoveSign4VietnameseString(tuKhoa)) ||
+                                            nv.Field<string>("SDT").ToString().ToLower().Contains(tuKhoa) ||
+                                            RemoveSign4VietnameseString(nv.Field<string>("DIACHI").ToString().ToLower()).Contains(RemoveSign4VietnameseString(tuKhoa)) ||
+                                            RemoveSign4VietnameseString(cv.Field<string>("TENCV").ToString().ToLower()).Contains(RemoveSign4VietnameseString(tuKhoa)) ||
+                                            cv.Field<double>("HESOLUONG").ToString().ToLower().Contains(tuKhoa)
+                                         )
+                                         orderby cv.Field<double>("HESOLUONG") descending //sắp xếp
+                                         select new
+                                         {
+                                             maNV = nv.Field<int>("MANV"),
+                                             tenNV = nv.Field<string>("TENNV"),
+                                             sdt = nv.Field<string>("SDT"),
+                                             diaChi = nv.Field<string>("DIACHI"),
+                                             maCV = cv.Field<int>("MACV"),
+                                             tenVC = cv.Field<string>("TENCV"),
+                                             heSoLuong = cv.Field<double>("HESOLUONG")
+                                         };
+                dtNhanVienChucVu = ToDataTable(nhaVienChucVuQuery);
+                dGVNhanVien.DataSource = dtNhanVienChucVu;
+                this.SetFormatDataGirdView();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Có lỗi xảy ra", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Hàm chuyển đổi IEmnumberable thành DataTable
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="items"></param>
+        /// <returns></returns>
+        public static DataTable ToDataTable<T>(IEnumerable<T> items)
+        {
+            // Create the result table, and gather all properties of a T        
+            DataTable table = new DataTable(typeof(T).Name);
+            PropertyInfo[] props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            // Add the properties as columns to the datatable
+            foreach (var prop in props)
+            {
+                Type propType = prop.PropertyType;
+
+                // Is it a nullable type? Get the underlying type 
+                if (propType.IsGenericType && propType.GetGenericTypeDefinition().Equals(typeof(Nullable<>)))
+                    propType = new NullableConverter(propType).UnderlyingType;
+
+                table.Columns.Add(prop.Name, propType);
+            }
+
+            // Add the property values per T as rows to the datatable
+            foreach (var item in items)
+            {
+                var values = new object[props.Length];
+                for (var i = 0; i < props.Length; i++)
+                    values[i] = props[i].GetValue(item, null);
+
+                table.Rows.Add(values);
+            }
+
+            return table;
+        }
+
+        private void dGVNhanVien_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = new DataGridViewRow();
+            row = dGVNhanVien.Rows[e.RowIndex];
+            this.manv = Convert.ToInt32(row.Cells["MANV"].Value.ToString());
+            txtTenNhanVien.Text = row.Cells[1].Value.ToString();
+            txtSDT.Text = row.Cells[2].Value.ToString();
+            txtDiaChi.Text = row.Cells[3].Value.ToString();
+            cboChucVu.Text = row.Cells[5].Value.ToString();
+        }
+        private static readonly string[] VietnameseSigns = new string[]
+        {
+
+            "aAeEoOuUiIdDyY",
+
+            "áàạảãâấầậẩẫăắằặẳẵ",
+
+            "ÁÀẠẢÃÂẤẦẬẨẪĂẮẰẶẲẴ",
+
+            "éèẹẻẽêếềệểễ",
+
+            "ÉÈẸẺẼÊẾỀỆỂỄ",
+
+            "óòọỏõôốồộổỗơớờợởỡ",
+
+            "ÓÒỌỎÕÔỐỒỘỔỖƠỚỜỢỞỠ",
+
+            "úùụủũưứừựửữ",
+
+            "ÚÙỤỦŨƯỨỪỰỬỮ",
+
+            "íìịỉĩ",
+
+            "ÍÌỊỈĨ",
+
+            "đ",
+
+            "Đ",
+
+            "ýỳỵỷỹ",
+
+            "ÝỲỴỶỸ"
+        };
+        /// <summary>
+        /// Hàm loại bỏ dấu tiếng việt
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string RemoveSign4VietnameseString(string str)
+        {
+            for (int i = 1; i < VietnameseSigns.Length; i++)
+            {
+                for (int j = 0; j < VietnameseSigns[i].Length; j++)
+                    str = str.Replace(VietnameseSigns[i][j], VietnameseSigns[0][i - 1]);
+            }
+            return str;
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            var nhanVien = new NHANVIEN();
+
+            nhanVien.TENNV = txtTenNhanVien.Text;
+            nhanVien.SDT = txtSDT.Text;
+            nhanVien.DIACHI = txtDiaChi.Text;
+            nhanVien.MACV = Convert.ToInt32(cboChucVu.SelectedValue.ToString());
+
+            using (var dbDemo = new dbDemoEntities())
+            {
+                dbDemo.NHANVIENs.Add(nhanVien);
+                dbDemo.SaveChanges();
+            }
+
+            MessageBox.Show("Thêm nhân viên thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.LoadData();
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            using (var dbDemo = new dbDemoEntities())
+            {
+                NHANVIEN nhanVien = (from nv in dbDemo.NHANVIENs where nv.MANV.Equals(this.manv) select nv).First();
+
+                nhanVien.TENNV = txtTenNhanVien.Text;
+                nhanVien.SDT = txtSDT.Text;
+                nhanVien.DIACHI = txtDiaChi.Text;
+                nhanVien.MACV = Convert.ToInt32(cboChucVu.SelectedValue.ToString());
+
+                dbDemo.SaveChanges();
+            }
+
+            MessageBox.Show("Cập nhật nhân viên thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.LoadData();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            using (var dbDemo = new dbDemoEntities())
+            {
+                NHANVIEN nhanVien = (from nv in dbDemo.NHANVIENs where nv.MANV.Equals(this.manv) select nv).First();
+
+                dbDemo.NHANVIENs.Remove(nhanVien);
+                dbDemo.SaveChanges();
+            }
+
+            MessageBox.Show("Xóa nhân viên thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.LoadData();
         }
     }
 }
